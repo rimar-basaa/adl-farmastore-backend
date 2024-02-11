f23b5a5134dcd0a8aa2ff4952e40d9b43f7b3661
Rimar Basaa <rimar.basaa@gmail.com>
2024-02-11 17:27:57 -0300

    se agregan filtros parametrizados

--- consultas.js
+++ consultas.js
@@ -10,11 +10,56 @@ const pool = new Pool({
     allowExitOnIdle: true
 });
 
-// Agregamos la función obtenerMedicamentos
-const obtenerMedicamentos = async ({ limits = 10}) => {
-    const consulta = "SELECT * FROM medicamentos LIMIT $1";
-    const { rows: medicamentos } = await pool.query(consulta, [limits]);
-    return medicamentos;
+
+const obtenerMedicamentos = async ({ order_by = id_ASC, limits = 10, page = 1 }) => {
+    const [campo, direccion] = order_by.split("_");
+    const offset = (page - 1) * limits;
+    const consultaFormateada = format('SELECT * FROM medicamentos ORDER BY %s %s LIMIT %s OFFSET %s', campo, direccion, limits, offset);    
+    const { rows } = await pool.query(consultaFormateada);
+    return rows;
 };
 
-module.exports = { obtenerMedicamentos }
+//consulta parametrizada
+const obtenerMedicamentosFiltrados = async ({ stock_min, precio_max }) => {
+    let filtros = [];
+    let values = [];
+
+    const agregarFiltro = (campo, comparador, valor) => {
+        values.push(valor);
+        const { length } = filtros;
+        console.log(length);
+        filtros.push(`${campo} ${comparador} $${length + 1}`);
+        console.log(filtros);
+    }
+
+    if (stock_min) agregarFiltro('stock', '>=', stock_min);
+    if (precio_max) agregarFiltro('precio', '<=', precio_max);
+
+    let consulta = "SELECT * FROM medicamentos";
+    if (filtros.length > 0) {
+        filtros = filtros.join(" AND ");
+        consulta += ` WHERE ${filtros}`;
+    };
+    const { rows } = await pool.query(consulta, values);
+    return rows;
+};
+
+module.exports = { obtenerMedicamentos, obtenerMedicamentosFiltrados };
+
+/*
+//consulta SIN parametrizar
+const obtenerMedicamentosFiltrados = async ({ stock_min, precio_max }) => {
+    let filtros = [];
+    if (stock_min) filtros.push(`stock >= ${stock_min}`);
+    if (precio_max) filtros.push(`precio <= ${precio_max}`);
+
+    let consulta = "SELECT * FROM medicamentos";
+    if (filtros.length > 0) {
+        filtros = filtros.join(" AND ");
+        consulta += ` WHERE ${filtros}`;
+    };
+    const { rows } = await pool.query(consulta);
+    return rows;
+};
+*/
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { obtenerMedicamentos } = require('./consultas');
+const { obtenerMedicamentos, obtenerMedicamentosFiltrados } = require('./consultas');
 
 
 app.get('/medicamentos', async (req, res) => {
@@ -8,6 +8,12 @@ app.get('/medicamentos', async (req, res) => {
     const medicamentos = await obtenerMedicamentos(queryStrings);
     res.status(200).json(medicamentos);
 });
+
+app.get('/medicamentos/filtros', async (req, res) => {
+    const queryStrings = req.query;
+    const medicamentos = await obtenerMedicamentosFiltrados(queryStrings);
+    res.status(200).json(medicamentos);
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Servidor corriendo... en puerto:${PORT}`));
